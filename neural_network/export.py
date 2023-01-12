7c67f2804515d3e5bd4e354b6b62ed7439357866
madshoeffer <mads.fogh2@gmail.com>
2023-01-12 19:36:00 +0100

    export weights as 1D and export biases

--- neural_network/export.py
+++ neural_network/export.py
@@ -4,13 +4,14 @@ from math import *
 
 header = """#include "ap_int.h"
 #include <cstdint>
-#include "ap_fixed.h"\n"""
+#include "ap_fixed.h"
+#define NN_data float\n"""
 
 
 def main():
     model = keras.models.load_model("keras_model")
 
-    filename = "weights.hpp"
+    filename = "weights2.hpp"
     open(filename,'w').close()
     file = open(filename,'a')
     file.write(header)
@@ -25,23 +26,24 @@ def main():
         else:
             file.write("#define n_layer"+str(i)+" "+str(neurons[i])+'\n')
     file.write("namespace weights{\n")
-         
   	## Retrieve network weights after training. Skip layer 0 (input layer)
     for w in range(1, len(model.layers)):
-        print(w)
-        file.write('const ap_fixed<32,24> layer'+str(w)+'_weights['+str(neurons[w-1])+']['+str(neurons[w])+'] = ')
+        file.write('const NN_data layer'+str(w)+'_weights['+str(neurons[w-1]*neurons[w])+'] = ')
         file.write('{')
-        for i in range(model.layers[w].weights[0].numpy().shape[0]):
-            file.write('{')
-            for j in range(model.layers[w].weights[0].numpy().shape[1]):
-                file.write(str(model.layers[w].weights[0].numpy()[i][j]))
-                if j != model.layers[w].weights[0].numpy().shape[1]-1:
+        for i in range(model.layers[w].weights[0].numpy().shape[1]):
+            # file.write('{')
+            for j in range(model.layers[w].weights[0].numpy().shape[0]):
+                file.write(str(model.layers[w].weights[0].numpy()[j][i]))
+                if (j == model.layers[w].weights[0].numpy().shape[0]-1) and (i == model.layers[w].weights[0].numpy().shape[1]-1):
+                    continue
+                else:
                     file.write(', ')
-            file.write('}')
-            if i != model.layers[w].weights[0].numpy().shape[0]-1:
-                file.write(', \n')
+            # file.write('}')
         file.write('};\n\n')
     file.write('}')
+    for i in range(1,len(model.layers)):
+        bias = ", ".join(map(lambda e:str(e), model.layers[i].get_weights()[1]))
+        file.write(f"NN_data bias_layer{i}[] = {{{bias}}};\n")
     file.close()
 
 if __name__ == "__main__":
