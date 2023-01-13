a18f2625ceeb68c194af8db3876bc34933b5dbdf
Karl-Emil Storm <karlemil.s.koldkjaer@gmail.com>
2023-01-13 18:37:02 +0100

    Updated model

--- neural_network/test.py
+++ neural_network/test.py
@@ -6,7 +6,7 @@ keras = tf.keras
 def main():
     model = keras.models.load_model("keras_model_small")
     
-    print(model.layers[7].get_weights())
+    print(model.layers[0].get_weights())
 
 
 if __name__ == "__main__":
